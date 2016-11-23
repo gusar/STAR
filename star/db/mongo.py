9b4be4a4ff33856e37f394674e323b01ed119714
Andy Lahs <andy.lahs@gmail.com>
2016-11-23 17:24:30 +0000

    Add mongo layer

---
+++ star/db/mongo.py
@@ -0,0 +1,28 @@
+import pymongo as pm
+
+
+class Mongo:
+
+    def __init__(self, mongodb_uri=None):
+        self.client = pm.MongoClient(mongodb_uri, connect=True)
+        self.db = None
+        self.collection = None
+
+    def set_db(self, db_name):
+        self.db = self.client[db_name]   
+
+    def set_collection(self, collection_name):
+        if self.db is not None:
+            self.collection = self.db[collection_name]
+        else:
+            raise MongoException("Database not defined")
+
+    def insert(self, data):
+        if self.collection:
+            self.collection.insert(data)
+        else:
+            raise MongoException("Collection not defined")
+
+
+class MongoException(Exception):
+    pass
